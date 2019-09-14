5d3efed9a81765e47e31d7bea8e380ec2b40e1a1
Andrey Helldar <helldar@ai-rus.com>
2019-09-15 02:59:55 +0300

    Added the ServiceContract

---
+++ src/Contracts/ServiceContract.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Helldar\BlacklistCore\Contracts;
+
+interface ServiceContract
+{
+    public function store(string $type, string $value);
+
+    public function check(string $value = null)
+    : string;
+
+    public function exists(string $value)
+    : bool;
+}
