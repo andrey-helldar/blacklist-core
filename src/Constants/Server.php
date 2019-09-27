01134a65de7a1336a6ff3aa12b95096c94d20fcc
Andrey Helldar <helldar@ai-rus.com>
2019-09-28 01:10:29 +0300

    Update Server.php

--- src/Constants/Server.php
+++ src/Constants/Server.php
@@ -4,13 +4,32 @@ namespace Helldar\BlacklistCore\Constants;
 
 class Server
 {
-    const BASE_URL         = 'https://blacklist.ai-rus.com';
+    const BASE_URL = 'https://blacklist.ai-rus.com';
 
-    const HEADERS          = ['Accept' => 'application/json'];
+    const HEADERS = ['Accept' => 'application/json'];
 
     const ROUTE_MIDDLEWARE = 'api';
 
-    const ROUTE_PREFIX     = 'api.blacklist.';
+    const ROUTE_PREFIX = 'api.blacklist.';
 
-    const URI              = 'api/blacklist';
+    const URI = 'api/blacklist';
+
+    public static function selfValues(): array
+    {
+        return [
+            '127.0.0.1',
+            self::host(),
+            self::email(),
+        ];
+    }
+
+    public static function host(): string
+    {
+        return parse_url(config('app.url'), PHP_URL_HOST);
+    }
+
+    public static function email(): string
+    {
+        return config('mail.from.address');
+    }
 }
