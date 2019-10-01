2c9bbe62a6d1b92869abb13b00f068a60d3a530f
Andrey Helldar <helldar@ai-rus.com>
2019-10-01 23:17:59 +0300

    Added a new helpers

---
+++ src/Helpers/Str.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Helldar\BlacklistCore\Helpers;
+
+use function preg_quote;
+use function preg_replace;
+
+class Str
+{
+    /**
+     * Convert the given string to upper-case.
+     *
+     * @param string $value
+     *
+     * @return string
+     */
+    public static function upper($value)
+    {
+        return mb_strtoupper($value, 'UTF-8');
+    }
+
+    /**
+     * Begin a string with a single instance of a given value.
+     *
+     * @param string $value
+     * @param string $prefix
+     *
+     * @return string
+     */
+    public static function start($value, $prefix)
+    {
+        $quoted = preg_quote($prefix, '/');
+
+        return $prefix . preg_replace('/^(?:' . $quoted . ')+/u', '', $value);
+    }
+}
