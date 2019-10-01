3cc43a1a044343aa16241c950aa4a5bdff4c8497
Andrey Helldar <helldar@ai-rus.com>
2019-10-01 22:33:39 +0300

    Added a new exceptions

---
+++ src/Exceptions/ExcludedBlockingDetectedException.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Helldar\BlacklistCore\Exceptions;
+
+use Exception;
+
+class ExcludedBlockingDetectedException extends Exception
+{
+    public function __construct(string $type = null)
+    {
+        $message = 'An attempt was made to block an excluded resource!';
+
+        parent::__construct($message, 400);
+    }
+}
