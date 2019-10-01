38759bc3ddfb004c5f213d384583e1c65c66def3
Andrey Helldar <helldar@ai-rus.com>
2019-10-01 11:09:06 +0300

    Create Validator.php

---
+++ src/Traits/Validator.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Helldar\BlacklistCore\Traits;
+
+use Helldar\BlacklistCore\Facades\Validator as Facade;
+
+trait Validator
+{
+    protected function validate(array $data, bool $is_require_type = true)
+    {
+        return Facade::validate($data, $is_require_type);
+    }
+}
