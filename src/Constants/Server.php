a23d05eeb880bd180d06bf76a3f0b62cc5d6fc76
Andrey Helldar <helldar@ai-rus.com>
2019-10-15 16:31:16 +0300

    Update Server.php

--- src/Constants/Server.php
+++ src/Constants/Server.php
@@ -4,8 +4,6 @@ namespace Helldar\BlacklistCore\Constants;
 
 class Server
 {
-    const BASE_URL         = 'https://blacklist.ai-rus.com';
-
     const HEADERS          = ['Accept' => 'application/json'];
 
     const ROUTE_MIDDLEWARE = 'api';
